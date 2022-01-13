627a7a6714f0a4e59e4d965fe1979a60e2adf884
webferrol <xurxo@webferrol.com>
2022-01-13 22:33:46 +0100

    Ejercicios de repaso de ficheros json, y funciones array: find, filter, map

---
+++ assets/repaso.js
@@ -0,0 +1,26 @@
+//////////////////////////////////////////////////
+///////////  EJERCICIO 1 ////////////////////////
+/////////////////////////////////////////////////
+
+//Importación de la variable datosPizzas de pizzas.js
+
+
+
+//Visualización en el elemento de id "renderizado-1" de los datos
+
+
+//////////////////////////////////////////////////
+///////////  EJERCICIO 2 ////////////////////////
+/////////////////////////////////////////////////
+
+//Leer el fichero pizzas.json
+
+
+//Buscar los elementos que tengan un precio superior a 2 € (array.prototype.filter)
+
+
+
+//Buscar el primer elemento que tenga un valor de 4 € (array.prototype.find)
+
+
+//Incrementar el precio de todos los productos en 10 € (array.prototype.map)
